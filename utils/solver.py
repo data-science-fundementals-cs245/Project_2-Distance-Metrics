90b1ac77f3ff51c44bf93e586871b977261b4211
jyl92 <ljy9792@hotmail.com>
2019-04-07 22:24:31 +0800

    Update solver.py

--- utils/solver.py
+++ utils/solver.py
@@ -10,7 +10,7 @@ class KNNSolver:
         self.neighbors = neighbors
 
     def train_and_decode(self, train_X, test_X, train_y, test_y):
-        print('Begin to train\ndistance type:\t{}number of neighbors'.format(self.distance, self.neighbors))
+        print('Begin to train\ndistance type:\t{}\tnumber of neighbors:\t{}'.format(self.distance, self.neighbors))
         knn = KNeighborsClassifier(n_neighbors=self.neighbors, metric=self.distance)
         knn.fit(train_X, train_y)
         acc = knn.score(test_X, test_y)
